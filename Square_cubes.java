9ee4c597e6ba1a2e54558e8cb9da3786910da049
Ebrahim Mohamed <72095424+em835@users.noreply.github.com>
2021-05-24 23:25:17 -0400

    Create Square_cubes.java

---
+++ Square_cubes.java
@@ -0,0 +1,61 @@
+/* Using only the programming techniques you learned in this lesson, write an application that calculates the squares and cubes of between two numbers and prints the resulting values in table format, as shown below. 
+number square cube  [ dont use any java built in method]
+enter two enters : 0  10
+
+0               0             0 
+1               1             1 
+2               4             8 
+.
+.
+.
+10            100         1000
+
+	
+
+*/
+	public class Main 
+	{
+	
+	public static void main(String[] args) 
+	{
+		
+		int x;
+		
+		System.out.println("number"+"\t"+"square"+"\t"+"cube");
+		
+		x = 0;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 1;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 2;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 3;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 4;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 5;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 6;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 7;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 8;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 9;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+		
+		x = 10;
+		System.out.printf("%d \t %d \t %d\n", (x), (x * x), (x * x * x));
+	
+	} 
+
+}
