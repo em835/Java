f117eba9974ad3844b1dcedf0b7400d260057297
Ebrahim Mohamed <72095424+em835@users.noreply.github.com>
2021-05-24 23:16:35 -0400

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -1,4 +1,4 @@
-
+/*
 (Display four patterns using loops) Use nested loops that display the following
 patterns in four separate programs:
 Pattern A        Pattern B  Pattern C    Pattern D
@@ -8,9 +8,9 @@ Pattern A        Pattern B  Pattern C    Pattern D
 1 2 3 4 	1 2 3       4 3 2 1           1 2 3
 1 2 3 4 5 	1 2 	  5 4 3 2 1 	    	    1 2
 1 2 3 4 5 6 	1 	6 5 4 3 2 1 	            1
+*/
 
 
-SOLUTION
 
 	public class Main
 	{
